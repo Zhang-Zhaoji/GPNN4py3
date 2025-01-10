7250245bb4f4822f5de45c2cbde93c481f48952f
Zhaoji Zhang <121226227+Zhang-Zhaoji@users.noreply.github.com>
2025-01-10 23:20:00 +0800

    add original codes
    ......may have some slightly hyperparameter change when running on Aliyun, but generally it is fine.

---
+++ original_codes/python/datasets/CAD120/__init__.py
@@ -0,0 +1,7 @@
+from . import cad120_config
+from . import cad120
+from . import metadata
+from . import parse_features
+from . import parse_features_prediction
+
+__all__ = ('cad120_config', 'cad120', 'metadata','parse_features','parse_features_prediction')
